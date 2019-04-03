f7e8ba04bdd430b808a3f90f8b53630df16b7832
yogg-soggot <nikit-iv777@ya.ru>
2019-04-03 21:59:55 +0300

    added package structure

--- app/src/main/java/org/styleru/the6hands/presentation/main/MainActivity.java
+++ app/src/main/java/org/styleru/the6hands/presentation/main/MainActivity.java
@@ -1,7 +1,9 @@
-package org.styleru.the6hands;
+package org.styleru.the6hands.presentation.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.styleru.the6hands.R;
 
 public class MainActivity extends AppCompatActivity {
 
